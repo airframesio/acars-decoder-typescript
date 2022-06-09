f4988b641a45a3a95a1a0d3a32ca4d573b198558
TLeconte <thierry.leconte35@free.fr>
2022-06-09 17:56:42 +0200

    use decodeStringCoordinates when possible, and use  POS item for every label

--- lib/plugins/Label_80.ts
+++ lib/plugins/Label_80.ts
@@ -163,20 +163,13 @@ export class Label_80 extends DecoderPlugin {
               break;
             }
             case 'POS': {
-              const posRegex = /^(?<latd>[NS])(?<lat>.+)(?<lngd>[EW])(?<lng>.+)/;
-              const posResult = match.groups.value.match(posRegex);
-              const latitude = (Number(posResult.groups.lat) / 100) * (posResult.groups.lngd === 'S' ? -1 : 1);
-              const longitude = (Number(posResult.groups.lng) / 100) * (posResult.groups.lngd === 'W' ? -1 : 1);
-              decodeResult.raw.aircraft_position = {
-                latitude,
-                longitude,
-              };
-              decodeResult.formatted.items.push({
-                type: 'aircraft_position',
-                code: 'POS',
-                label: this.descriptions[match.groups.field],
-                value: `${(Number(posResult.groups.lat) / 100).toPrecision(5)} ${posResult.groups.latd}, ${(Number(posResult.groups.lng) / 100).toPrecision(5)} ${posResult.groups.lngd}`,
-              });
+	      decodeResult.raw.position = this.decodeStringCoordinates(match.groups.value);
+	      decodeResult.formatted.items.push({
+	        type: 'position',
+	        code: 'POS' ,
+	        label: 'Position',
+	        value: this.coordinateString(decodeResult.raw.position),
+	      });
               break;
             }
             case 'SWND': {
