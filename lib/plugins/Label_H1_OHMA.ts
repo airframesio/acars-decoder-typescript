88f20d9d851938eaf2b5ce1e73088b0c524899c9
Mark Bumiller <mark.bumiller@gmail.com>
2024-08-02 09:02:59 -0400

    format fix

--- lib/plugins/Label_H1_OHMA.ts
+++ lib/plugins/Label_H1_OHMA.ts
@@ -23,24 +23,24 @@ export class Label_H1_OHMA extends DecoderPlugin {
     const data = message.text.slice(4);
 
     try {
-    const compressedBuffer = Buffer.from(data, 'base64');
-    const decompress = new zlib.Inflate({windowBits: 15});
-    decompress.write(compressedBuffer);
-    decompress.flush(zlib.constants.Z_SYNC_FLUSH);
-    const result = decompress.read();
-    const jsonText = result.toString();
-    const json = JSON.parse(jsonText);
-    const ohmaMsg = JSON.parse(json.message);
+      const compressedBuffer = Buffer.from(data, 'base64');
+      const decompress = new zlib.Inflate({windowBits: 15});
+      decompress.write(compressedBuffer);
+      decompress.flush(zlib.constants.Z_SYNC_FLUSH);
+      const result = decompress.read();
+      const jsonText = result.toString();
+      const json = JSON.parse(jsonText);
+      const ohmaMsg = JSON.parse(json.message);
 
-    decodeResult.decoded = true;
-    decodeResult.decoder.decodeLevel = 'full';
-    decodeResult.raw.ohma = jsonText;
-    decodeResult.formatted.items.push({
-      type: 'ohma',
-      code: 'OHMA' ,
-      label: 'OHMA Downlink',
-      value: JSON.stringify(ohmaMsg, null, 2)
-    });
+      decodeResult.decoded = true;
+      decodeResult.decoder.decodeLevel = 'full';
+      decodeResult.raw.ohma = jsonText;
+      decodeResult.formatted.items.push({
+        type: 'ohma',
+        code: 'OHMA' ,
+        label: 'OHMA Downlink',
+        value: JSON.stringify(ohmaMsg, null, 2)
+      });
     } catch {
       // Unknown
       if (options.debug) {
