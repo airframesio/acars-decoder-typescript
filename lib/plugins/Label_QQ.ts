5ac380c1dee98db3bed3630cf3095b0182f541cd
Rajan Patel <rpatel3001@gmail.com>
2024-10-09 02:23:45 +0000

    more QQ

--- lib/plugins/Label_QQ.ts
+++ lib/plugins/Label_QQ.ts
@@ -1,5 +1,7 @@
 import { DecoderPlugin } from '../DecoderPlugin';
 import { DecodeResult, Message, Options } from '../DecoderPluginInterface';
+import { CoordinateUtils } from '../utils/coordinate_utils';
+import { ResultFormatter } from '../utils/result_formatter';
 
 export class Label_QQ extends DecoderPlugin {
   name = 'label-qq';
@@ -14,48 +16,44 @@ export class Label_QQ extends DecoderPlugin {
     const decodeResult = this.defaultResult();
     decodeResult.decoder.name = this.name;
     
-    decodeResult.raw.origin = message.text.substring(0, 4);
-    decodeResult.raw.destination = message.text.substring(4, 8);
+    ResultFormatter.departureAirport(decodeResult, message.text.substring(0, 4));
+    ResultFormatter.arrivalAirport(decodeResult, message.text.substring(4, 8));
+
     decodeResult.raw.wheels_off = message.text.substring(8, 12);
 
-    if (message.text.substring(12, 18) === "\n001FE") {
-        decodeResult.raw.day_of_month = message.text.substring(18, 20);
-        decodeResult.raw.wheels_off = message.text.substring(20, 26);
-        decodeResult.raw.position.latitude = message.text.substring(26, 33);
-        decodeResult.raw.position.longitude = message.text.substring(33, 41);
-        decodeResult.remaining.text = message.text.substring(41, 44);
+    if (message.text.substring(12, 19) === "\r\n001FE") {
+        decodeResult.raw.day_of_month = message.text.substring(19, 21);
+        decodeResult.raw.wheels_off = message.text.substring(21, 27);
+        decodeResult.raw.position.latitude = message.text.substring(27, 34);
+        decodeResult.raw.position.longitude = message.text.substring(34, 42);
+        decodeResult.remaining.text = message.text.substring(42, 45);
         if (decodeResult.remaining.text !== "---") {
-            decodeResult.raw.groundspeed = message.text.substring(44, 47);
+            ResultFormatter.groundspeed(decodeResult, message.text.substring(45, 48));
         } else {
-            decodeResult.remaining.text += "," + message.text.substring(44, 47);
+            ResultFormatter.unknown(decodeResult, message.text.substring(45, 48));
         }
-        decodeResult.remaining.text += "," + message.text.substring(47);
+        ResultFormatter.unknown(decodeResult, message.text.substring(48));
     } else {
         decodeResult.remaining.text = message.text.substring(12);
     }
 
     decodeResult.formatted.description = 'OFF Report';
 
-    decodeResult.formatted.items = [
-      {
-        type: 'origin',
-        code: 'ORG',
-        label: 'Origin',
-        value: decodeResult.raw.origin,
-      },
-      {
-        type: 'destination',
-        code: 'DST',
-        label: 'Destination',
-        value: decodeResult.raw.destination,
-      },
-      {
+    decodeResult.formatted.items.push({
         type: 'wheels_off',
         code: 'WOFF',
         label: 'Wheels OFF',
         value: decodeResult.raw.wheels_off,
-      }
-    ];
+    });
+
+    if (decodeResult.raw.position.latitude) {
+        decodeResult.formatted.items.push({
+            type: 'aircraft_position',
+            code: 'POS',
+            label: 'Aircraft Position',
+            value: CoordinateUtils.coordinateString(decodeResult.raw.position),
+        });
+    }
 
     decodeResult.decoded = true;
     if(decodeResult.remaining.text === "") 
