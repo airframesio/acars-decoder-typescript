517ef59f90c06807a12bb92ac1bac4fa45cece3f
Mark Bumiller <mark.bumiller@gmail.com>
2024-10-17 17:58:51 -0400

    remove copied test

--- lib/plugins/Label_24_Slash.test.ts
+++ lib/plugins/Label_24_Slash.test.ts
@@ -36,24 +36,6 @@ describe('Label_24_Slash', () => {
     expect(decodeResult.remaining.text).toBe('3374');
   });
 
-  
-  test('2valid', () => {
-    // https://app.airframes.io/messages/3439806391
-    const text = '/241710/1021/04WM/34962/N53.13/E001.33/3374/1056/';
-    const decodeResult = plugin.decode({ text: text });
-    console.log(JSON.stringify(decodeResult, null, 2));
-    expect(decodeResult.decoded).toBe(true);
-    expect(decodeResult.decoder.decodeLevel).toBe('partial');
-    expect(decodeResult.raw.message_timestamp).toBe(1729160460);
-    expect(decodeResult.raw.flight_number).toBe('04WM');
-    expect(decodeResult.raw.altitude).toBe(34962);
-    expect(decodeResult.raw.position.latitude).toBe(53.13);
-    expect(decodeResult.raw.position.longitude).toBe(1.33);
-    expect(decodeResult.raw.eta_time).toBe(39360);
-    expect(decodeResult.formatted.items.length).toBe(3);
-    expect(decodeResult.remaining.text).toBe('3374');
-  });
-
   test('does not decode invalid', () => {
 
     const text = '/ Bogus message';
