993e4efa8ed812d49197929f940d9be482219c70
Mark Bumiller <mark.bumiller@gmail.com>
2024-10-17 17:57:20 -0400

    Adding label 24 / parsing

---
+++ lib/plugins/Label_24_Slash.test.ts
@@ -0,0 +1,66 @@
+import { MessageDecoder } from '../MessageDecoder';
+import { Label_24_Slash } from './Label_24_Slash';
+
+describe('Label_24_Slash', () => {
+  let plugin: Label_24_Slash;
+
+  beforeEach(() => {
+    const decoder = new MessageDecoder();
+    plugin = new Label_24_Slash(decoder);
+  });
+
+  test('matches qualifiers', () => {
+    expect(plugin.decode).toBeDefined();
+    expect(plugin.name).toBe('label-24-slash');
+    expect(plugin.qualifiers).toBeDefined();
+    expect(plugin.qualifiers()).toEqual({
+      labels: ['24'],
+      preambles: ['/'],
+    });
+  });
+
+
+  test('valid', () => {
+    // https://app.airframes.io/messages/3439806391
+    const text = '/241710/1021/04WM/34962/N53.13/E001.33/3374/1056/';
+    const decodeResult = plugin.decode({ text: text });
+    expect(decodeResult.decoded).toBe(true);
+    expect(decodeResult.decoder.decodeLevel).toBe('partial');
+    expect(decodeResult.raw.message_timestamp).toBe(1729160460);
+    expect(decodeResult.raw.flight_number).toBe('04WM');
+    expect(decodeResult.raw.altitude).toBe(34962);
+    expect(decodeResult.raw.position.latitude).toBe(53.13);
+    expect(decodeResult.raw.position.longitude).toBe(1.33);
+    expect(decodeResult.raw.eta_time).toBe(39360);
+    expect(decodeResult.formatted.items.length).toBe(3);
+    expect(decodeResult.remaining.text).toBe('3374');
+  });
+
+  
+  test('2valid', () => {
+    // https://app.airframes.io/messages/3439806391
+    const text = '/241710/1021/04WM/34962/N53.13/E001.33/3374/1056/';
+    const decodeResult = plugin.decode({ text: text });
+    console.log(JSON.stringify(decodeResult, null, 2));
+    expect(decodeResult.decoded).toBe(true);
+    expect(decodeResult.decoder.decodeLevel).toBe('partial');
+    expect(decodeResult.raw.message_timestamp).toBe(1729160460);
+    expect(decodeResult.raw.flight_number).toBe('04WM');
+    expect(decodeResult.raw.altitude).toBe(34962);
+    expect(decodeResult.raw.position.latitude).toBe(53.13);
+    expect(decodeResult.raw.position.longitude).toBe(1.33);
+    expect(decodeResult.raw.eta_time).toBe(39360);
+    expect(decodeResult.formatted.items.length).toBe(3);
+    expect(decodeResult.remaining.text).toBe('3374');
+  });
+
+  test('does not decode invalid', () => {
+
+    const text = '/ Bogus message';
+    const decodeResult = plugin.decode({ text: text });
+
+    expect(decodeResult.decoded).toBe(false);
+    expect(decodeResult.decoder.decodeLevel).toBe('none');
+    expect(decodeResult.message.text).toBe(text);
+  });
+});
---
+++ lib/plugins/Label_24_Slash.ts
@@ -0,0 +1,56 @@
+import { DateTimeUtils } from '../DateTimeUtils';
+import { DecoderPlugin } from '../DecoderPlugin';
+import { DecodeResult, Message, Options } from '../DecoderPluginInterface';
+import { ResultFormatter } from '../utils/result_formatter';
+
+// Position Report
+export class Label_24_Slash extends DecoderPlugin {
+  name = 'label-24-slash';
+
+  qualifiers() { // eslint-disable-line class-methods-use-this
+    return {
+      labels: ['24'],
+      preambles: ['/'],
+    };
+  }
+
+  decode(message: Message, options: Options = {}) : DecodeResult {
+    const decodeResult = this.defaultResult();
+    decodeResult.decoder.name = this.name;
+    decodeResult.formatted.description = 'Position Report';
+    decodeResult.message = message;
+
+    const fields = message.text.split('/');
+
+    if (fields.length == 10 && fields[0] == '' && fields[9] == '') { // begin and ends with `/`
+      const mmddyy = fields[1].substring(4,6) + fields[1].substring(2,4) + fields[1].substring(0,2); // YYDDMM
+      const hhmmss = fields[2] + '00';
+      decodeResult.raw.message_timestamp = DateTimeUtils.convertDateTimeToEpoch(hhmmss,mmddyy);
+      ResultFormatter.flightNumber(decodeResult, fields[3]);
+      ResultFormatter.altitude(decodeResult, Number(fields[4]));
+      const lat = fields[5];
+      const lon = fields[6];
+      const position = {
+        latitude: (lat[0] === 'N' ? 1 : -1) * Number(lat.substring(1)),
+        longitude: (lon[0] === 'E' ? 1 : -1) * Number(lon.substring(1)),
+      };
+      ResultFormatter.position(decodeResult, position);
+      ResultFormatter.eta(decodeResult, DateTimeUtils.convertHHMMSSToTod(fields[8]+'00'));
+      decodeResult.remaining.text = fields[7];
+
+      decodeResult.decoded = true;
+      decodeResult.decoder.decodeLevel = 'partial';
+    } else {
+      // Unknown!
+      if(options.debug) {
+        console.log(`DEBUG: ${this.name}: Unknown variation. Field count: ${fields.length}`);
+      }
+      decodeResult.decoded = false;
+      decodeResult.decoder.decodeLevel = 'none';
+    }
+    return decodeResult;
+  }
+}
+
+export default {};
+
