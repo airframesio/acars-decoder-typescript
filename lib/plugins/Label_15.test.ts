1af71a8119d8465bb1335dfccd2aa95ab2467143
Mark Bumiller <mark.bumiller@gmail.com>
2024-11-12 08:07:28 -0500

    Adding OAT to label 15
    Adding code for missing alt
    adding code for long variant

--- lib/plugins/Label_15.test.ts
+++ lib/plugins/Label_15.test.ts
@@ -19,32 +19,62 @@ describe('Label_15', () => {
         });
     });
 
-    test('decodes variant 1', () => {
+    test('decodes short variant missing ???', () => {
         const text = '(2N39413W 73354--- 66349-43(Z'
         const decodeResult = plugin.decode({ text: text });
         
         expect(decodeResult.decoded).toBe(true);
         expect(decodeResult.decoder.decodeLevel).toBe('partial');
-        expect(decodeResult.formatted.items.length).toBe(2);
+        expect(decodeResult.formatted.items.length).toBe(3);
         expect(decodeResult.formatted.items[0].label).toBe('Aircraft Position');
         expect(decodeResult.formatted.items[0].value).toBe('39.688 N, 73.590 W');
         expect(decodeResult.formatted.items[1].label).toBe('Altitude');
         expect(decodeResult.formatted.items[1].value).toBe('34900 feet');
-        expect(decodeResult.remaining.text).toBe('--- 66-43');
+        expect(decodeResult.formatted.items[2].label).toBe('Outside Air Temperature (C)');
+        expect(decodeResult.formatted.items[2].value).toBe('-43 degrees');
+        expect(decodeResult.remaining.text).toBe('--- 66');
     });
 
-    test('decodes variant 2', () => {
+    test('decodes short variant all fields', () => {
         const text = '(2N40492W 77179248 99380-53(Z'
+        const decodeResult = plugin.decode({ text: text });
+        
+        expect(decodeResult.decoded).toBe(true);
+        expect(decodeResult.decoder.decodeLevel).toBe('partial');
+        expect(decodeResult.formatted.items.length).toBe(3);
+        expect(decodeResult.formatted.items[0].label).toBe('Aircraft Position');
+        expect(decodeResult.formatted.items[0].value).toBe('40.820 N, 77.298 W');
+        expect(decodeResult.formatted.items[1].label).toBe('Altitude');
+        expect(decodeResult.formatted.items[1].value).toBe('38000 feet');
+        expect(decodeResult.formatted.items[2].label).toBe('Outside Air Temperature (C)');
+        expect(decodeResult.formatted.items[2].value).toBe('-53 degrees');
+        expect(decodeResult.remaining.text).toBe('248 99');
+    });
+    
+    test('decodes short variant missing alt', () => {
+        const text = '(2N39269W 77374--- 42---- 5(Z'
         const decodeResult = plugin.decode({ text: text });
         
         expect(decodeResult.decoded).toBe(true);
         expect(decodeResult.decoder.decodeLevel).toBe('partial');
         expect(decodeResult.formatted.items.length).toBe(2);
         expect(decodeResult.formatted.items[0].label).toBe('Aircraft Position');
-        expect(decodeResult.formatted.items[0].value).toBe('40.820 N, 77.298 W');
-        expect(decodeResult.formatted.items[1].label).toBe('Altitude');
-        expect(decodeResult.formatted.items[1].value).toBe('38000 feet');
-        expect(decodeResult.remaining.text).toBe('248 99-53');
+        expect(decodeResult.formatted.items[0].value).toBe('39.448 N, 77.623 W');
+        expect(decodeResult.formatted.items[1].label).toBe('Outside Air Temperature (C)');
+        expect(decodeResult.formatted.items[1].value).toBe('-5 degrees');
+        expect(decodeResult.remaining.text).toBe('--- 42');
+    });
+    
+    test('decodes long variant', () => {
+        const text = '(2N39018W 77284OFF11112418101313--------(Z'
+        const decodeResult = plugin.decode({ text: text });
+        
+        expect(decodeResult.decoded).toBe(true);
+        expect(decodeResult.decoder.decodeLevel).toBe('partial');
+        expect(decodeResult.formatted.items.length).toBe(1);
+        expect(decodeResult.formatted.items[0].label).toBe('Aircraft Position');
+        expect(decodeResult.formatted.items[0].value).toBe('39.030 N, 77.473 W');
+        expect(decodeResult.remaining.text).toBe('OFF11112418101313--------');
     });
 
     test('does not decode Label 15 <invalid>', () => {
